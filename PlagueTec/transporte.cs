a4fd675047269e34c44827bb17068039d0c86190
Alumno <Alumno>
2017-04-12 13:49:03 -0500

    IMPLEMENTACIÓN DE FUNCIONES

--- PlagueTec/transporte.cs
+++ PlagueTec/transporte.cs
@@ -8,45 +8,53 @@ namespace PlagueTec
 {
     class transporte
     {
-        string[] type_tipo = { "terrestre", "aereo", "maritimo" };
-        int[] capacidades = {15 ,35 , 55 };
+        public int[,] array2D = new int[,] { { -1, 5, -1, 9, -1 }, { -1, -1, 4, -1, -1 }, { -1, -1, -1, 2, -1 }, { 9, 6, -1, -1, 3 }, { -1, -1, -1, 3, -1 } }; //(duración) ESTO PONEN TODAS LAS FILAS DE LA TABLA LA CUAL EL USUARIO LUEGO PONE POR TECLADO LAS RUTAS
+
+        string[] type_tipo = { "terrestre", "aereo", "maritimo" };      //Declaración de TIPO
+        int[] capacidades = { 15, 35, 55 };     //declaración de capacidades
         Random rnd = new Random();
 
+        public string tipo; //A ESTÁ 
+        public int capacidad; // YA ESTÁ
+        public bool finish_route = false;
+        public int tiempo; //TIEMPO QUE LLEO EN EL PRESENTE 
+        public int duracion;  //DURACIÓN DEL VIAJE  YA ESTÁ 
+        public string ruta;     //SI ESTÁ HABILITADA O NO >:V' YA ESTÁ 
+                                // DURACIÓN ES (EJEMPLO DE LIMA A HUARAZ SON 7 HORAS)      TIEMPO ES (EJEM EL VIAJE DE LIMA A HUARAZ TARDÓ 8 HORAS)
 
-        public string tipo;
-        public int capacidad;
-        public int tiempo;
-        public int duracion;
-        public string ruta;
-        public string informacion;
+        Random n = new Random((int)DateTime.Now.Ticks & 0x0000FFFF); //
 
-        Random n = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
-
-        public transporte()
+        public transporte()         //FUNCIÓN
         {
-            int num = n.Next(type_tipo.Length);
-            tipo = type_tipo[num];
+            int num = n.Next(type_tipo.Length);   //número de pasajeros
+            tipo = type_tipo[num];      /*para que eliga un transporte random de los atributor*/
             capacidad = capacidades[num];
-           
+
         }
-        
-       public void update()
+
+        public void update()
         {
             if (tiempo >= duracion)
             {
-                //Pais destino que reciba los pasajeros
+                finish_route = true;
+                //Pais destino que reciba los pasajero
             }
-                tiempo++;
+
+
+
+
+            tiempo++;
         }
-
     }
-     /* 1 SEGUNDO ES 4 CICLOS
-      * 24 SEGUN  1 AÑO
-      * 12 SEGUN    6 MESES
-      * 2 SEGUN     1 MES
-      * 1 SEGUND        15 DÍAS */
+}
+
+/* 1 SEGUNDO ES 4 CICLOS
+ * 24 SEGUN  1 AÑO
+ * 12 SEGUN    6 MESES
+ * 2 SEGUN     1 MES
+ * 1 SEGUND        15 DÍAS */
 
 
-        
-    }
+
+
 
