d72833625126835e88dedf3baa15aed89ea5ed8c
Alumno <Alumno>
2017-04-12 11:33:11 -0500

    virus create

---
+++ PlagueTec/Virus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlagueTec
+{
+    class Virus
+    {
+        string name;
+        float letalidad;
+        float tiemp_de_reproduccion;
+        float hereditario;
+
+
+
+        public Virus()
+        {
+            this.name = "karius";
+            this.letalidad = 0;
+            this.tiemp_de_reproduccion = 0;
+            this.hereditario = 0.20f;
+
+        }
+
+        public void VirusAttack()
+        {
+            /* if (letalidad=100)
+             persona=0*/
+
+        }
+        public void infeccion ()
+        {
+           
+        }
+    }
+}
